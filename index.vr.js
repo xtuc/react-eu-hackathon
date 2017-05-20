ffc6250b60678135864d3ac755d6cd9afea032d1
Federico Zivolo <fzivolo@quid.com>
2017-05-20 10:30:33 +0200

    eart

--- index.vr.js
+++ index.vr.js
@@ -4,28 +4,32 @@ import {
   asset,
   Pano,
   Text,
+  Model,
+  PointLight,
   View,
 } from 'react-vr';
+
 
 export default class hackathon extends React.Component {
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
+        <PointLight style={{color:'white', transform:[{translate : [0, 0, 0]}]}} />
+
+        <Model
+          lit
           style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+            translateX: 100,
+            translateY: 100,
+            scaleX: 0.5,
+            scaleY: 0.5,
+          }}
+          source={{
+            obj: asset('earth.obj'),
+            mtl: asset('earth.mtl'),
+          }}
+        />
       </View>
     );
   }
